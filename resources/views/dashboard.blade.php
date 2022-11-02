0acb91d74b01ed94a45d6fbbecd4f10a014e868c
NicholasWarui <nyamunick99@gmail.com>
2022-11-02 19:15:12 +0300

    commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,7 @@
 @extends('master')
 
 @section('content')
+
  <h4 class="pt-2">Welcome, {{ Auth::user()->Fname }} {{ Auth::user()->Lname }} </h4>
 
    @if (Auth::user()->role == 'Manager')
@@ -14,28 +15,28 @@
 
       <h5>Dashboard</h5>
       <div class="cardviews">
-            <div class="val-box">
+            <div class="val-box shadow p-3">
                <i class="fas fa-users"></i>
                   <div>
                      <h3>8,267</h3>
                      <span>New Users</span>
                   </div>
             </div>
-            <div class="val-box">
+            <div class="val-box shadow p-3">
                <i class="fas fa-users"></i>
                   <div>
                      <h3>8,267</h3>
                      <span>New Users</span>
                   </div>
             </div>
-            <div class="val-box">
+            <div class="val-box shadow p-3">
                <i class="fas fa-users"></i>
                   <div>
                      <h3>8,267</h3>
                      <span>New Users</span>
                   </div>
             </div>
-            <div class="val-box">
+            <div class="val-box shadow p-3">
                <i class="fas fa-users"></i>
                   <div>
                      <h3>8,267</h3>
@@ -46,5 +47,14 @@
    </div>
    
    @endif
-   
+   <div class="row">
+      <div class="col-lg-6">
+         <div class="card">
+            <div class="card-header border-0">
+            <h2 class="card-title">Chart</h2>
+
+            </div>
+         </div>
+      </div>
+   </div>
 @endsection
